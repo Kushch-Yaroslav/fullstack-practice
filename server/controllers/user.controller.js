691a1eaba6cd630917b30ae62875ee7a36c20c4e
freshcodeAlice <freshjs.ofte@gmail.com>
2023-01-26 15:24:59 +0200

    signIn done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,11 +1,13 @@
+const bcrypt = require('bcryptjs');
 const {User} = require('../models');
 const NotFoundError = require('../errors/NotFound');
+const InvalidDataError = require('../errors/InvalidDataError');
 
 module.exports.signUpUser = async (req, res, next) => {
     try {
         const {body, passwordHash} = req;
         const createdUser = await User.create({...body, passwordHash});
-        res.status(201).send(createdUser);
+        res.status(201).send({data: createdUser});
     } catch(error) {
         next(error);
     }
@@ -33,7 +35,14 @@ module.exports.signInUser = async (req, res, next) => {
                 email: email
         });
         if (foundUser) {
-            /// порівняння паролів
+            const result = await bcrypt.compare(password, foundUser.passwordHash);
+            // Або пароль правильний, або ні.
+            console.log(result); 
+            if (result) {
+                res.status(200).send({data: foundUser});
+            } else {
+               throw new InvalidDataError('Invalid credentials')
+            }
         } else {
             throw new NotFoundError('User not found');
         }
