2aca5aca565c858c9c7777db6a5933f3eb3e2f07
freshcodeAlice <freshjs.ofte@gmail.com>
2023-01-30 16:29:19 +0200

    axios interseptors

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,7 +5,46 @@ const httpClient = axios.create({
 });
 
 
+httpClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if(token) {
+        config.headers = {
+            ...config.headers,
+            Authorization: `Bearer ${token}`
+        }
+    }
+    return config;
+}, (err) => Promise.reject(err));
+
+httpClient.interceptors.response.use((response) => {
+    if(response.data.token) {
+        const {data: {token}} = response;
+        localStorage.setItem('token', token);
+    }
+    return response;
+})
+
+
+/* Auth api */
 
 export const signIn = async (userData) => await httpClient.post('/users/sign-in', userData);
 
 export const signUp = async (userData) => await httpClient.post('/users/sign-up', userData);
+
+
+/* Chat api  
+
+*/
+
+export const createChat = async (data) => await httpClient.post('/chats/', data);
+
+export const addNewMessage = async (chatId, data) => await httpClient.post(`/chats/${chatId}`, data);
+
+export const getChatWithMessages = async (chatId) => await httpClient.get(`/chats/${chatId}`);
+
+
+export const getUserChats = async () => await httpClient.get('/chats/user/');
+
+export const addUserToChat = async (chatId, userId) => await httpClient.put(`/chats/${chatId}`, {userId});
+
+
