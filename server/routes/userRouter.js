aa893db700fc8fee19db7fb6f7174831e78b54a7
freshcodeAlice <freshjs.ofte@gmail.com>
2023-01-31 15:48:26 +0200

    route fixed

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -7,6 +7,7 @@ const userRouter = Router();
 userRouter.post('/sign-up', hashPass, UserController.signUpUser) // signUp
 userRouter.post('/sign-in', UserController.signInUser) // singIn
 userRouter.get('/', checkToken, UserController.getOneUser) // getOne user
+userRouter.post('/refresh', UserController.refreshSession); // refresh session
 
 
 module.exports = userRouter;
